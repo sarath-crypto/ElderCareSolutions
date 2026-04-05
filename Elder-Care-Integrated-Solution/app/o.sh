9b5272830f642a7ad0f8fc85fca072f55574b277
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2026-04-05 09:44:43 +0530

    Delete Elder-Care-Integrated-Solution/app directory

--- Elder-Care-Integrated-Solution/app/o.sh
+++
@@ -1,9 +0,0 @@
-g++ -c motiondetector.cpp 	`pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c scanner.cpp        	`pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c filters.cpp        	-ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c udps.cpp           	`pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c syscam.cpp         	`pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1 
-g++ -c global.cpp 	  	`pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c fft.cpp            	`pkg-config --cflags --libs opencv4` -lkissfft-float -Wno-psabi  -ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c ds18b20.cpp		-lwiringPi -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1
-g++ -c fb.cpp             	`pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O1
