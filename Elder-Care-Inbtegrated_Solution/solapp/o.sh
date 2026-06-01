8a00232bffdfcce1a7f8ed94e0304f20e4a52754
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2026-06-01 17:55:54 +0530

    Delete Elder-Care-Inbtegrated_Solution directory

--- Elder-Care-Inbtegrated_Solution/solapp/o.sh
+++
@@ -1,7 +0,0 @@
-g++ -c ../ecsysapp/global.cpp `pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 -I../ecsysapp 
-g++ -c udps.cpp               -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
-g++ -c fft.cpp -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
-g++ -c filters.cpp -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
-
-
-
