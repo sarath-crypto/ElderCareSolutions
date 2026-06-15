0642671a049779eeb4763b4aadf1181ba00121b3
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2026-06-15 16:04:09 +0530

    Delete Elder-Care-Integrated_Solution directory

--- Elder-Care-Integrated_Solution/solapp/l.sh
+++
@@ -1,7 +0,0 @@
-while true
-do
-	tail -1000 /var/log/syslog | grep solapp
-	ps -alx | grep solapp
-        sleep 2
-done
-
--- Elder-Care-Integrated_Solution/tools/genwav/gen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python genwav.py
-ffmpeg -i mon.wav -ac 2 on.wav
-ffmpeg -i moff.wav -ac 2 off.wav
-rm mon.wav -rf
-rm moff.wav -rf
