b9af0d0943928a403658ea1ac81a166f3f391fe6
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2026-06-15 16:06:43 +0530

    Ir blaster over wifi integrated

---
+++ Elder-Care-Integrated_Solution/solapp/o.sh
@@ -0,0 +1,5 @@
+g++ -c ../ecsysapp/global.cpp `pkg-config --cflags --libs opencv4` -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 -I../ecsysapp 
+g++ -c udps.cpp               -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
+g++ -c tcpc.cpp               -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
+g++ -c fft.cpp -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
+g++ -c filters.cpp -Wno-psabi -ldl -lm -Wall -Wextra -Wunused-variable -O0 
